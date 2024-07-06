8025cd43d383bb418179e35e77ce202154203489
karim <O34377@bankfab.com>
2024-07-06 13:38:52 +0300

    Project Intialization

---
+++ Yassir-Home-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yassir-Home-Task
+//
+//  Created by Karim Soliman on 06/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
